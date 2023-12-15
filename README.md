e8b677218bfe33cd6ddc4fa8b9f07d49f34a8040
dinesh <deepakdinesh990@gmail.com>
2023-12-15 18:48:32 +0530

    1. completed snake game.
    2. Added gh-pages config.

--- README.md
+++ README.md
@@ -1,70 +1,17 @@
-# Getting Started with Create React App
+# Snake game using Reactjs.
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Use the keys UP, DOWN, RIGHT, and LEFT to make the snake grow longer.
+You can't stop the snake or make it go backwards.
+The game ends when the snake into itself.
+The goal is to make the snake as large as possible before that happens.
+The score is determined by the number of segments your final snake is.
+The line acts as a blockade and the player who lasted longer was the winner.
 
-## Available Scripts
 
-In the project directory, you can run:
+Play Game :  [Click TO PlaY](https://rdinesh1667.github.io/weather-app/) <br>
 
-### `npm start`
+# GAME Lock Like<br>
+![Weather Report](https://raw.github.com/Rdinesh1667/weather-app/master/src/images/weather-app-1.png) <br>
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+# After Game End <br>
+![Weather Report](https://raw.github.com/Rdinesh1667/weather-app/master/src/images/weather-app-2.png) <br>
--- src/App.js
+++ src/App.js
@@ -1,25 +1,146 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const ROWS = 25;
+const COLS = 25;
+const INITIAL_SNAKE = [{ row: 12, col: 12 }];
+const INITIAL_DIRECTION = 'RIGHT';
+
+const generateFood = () => {
+  return {
+    row: Math.floor(Math.random() * ROWS),
+    col: Math.floor(Math.random() * COLS),
+  };
+};
+
+const App = () => {
+  const [snake, setSnake] = useState(INITIAL_SNAKE);
+  const [direction, setDirection] = useState(INITIAL_DIRECTION);
+  const [food, setFood] = useState(generateFood());
+  const [gameOver, setGameOver] = useState(false);
+  const [isPass, setIsPass] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const checkCollision = (snake) => {
+    const head = snake[0];
+    return (
+      snake.slice(1).some((segment) => segment.row === head.row && segment.col === head.col) ||
+      head.row < 0 ||
+      head.row >= ROWS ||
+      head.col < 0 ||
+      head.col >= COLS
+    );
+  };
+
+
+  const resetGame = () => {
+    setSnake(INITIAL_SNAKE);
+    setDirection(INITIAL_DIRECTION);
+    setFood(generateFood());
+    setGameOver(false);
+    setScore(0)
+  };
+
+  useEffect(() => {
+    if (!gameOver && !isPass) {
+      const moveSnake = () => {
+        const newSnake = snake.map((segment) => ({ ...segment }));
+
+        const head = { ...newSnake[0] };
+
+        switch (direction) {
+          case 'UP':
+            head.row = (head.row - 1 + ROWS) % ROWS;
+            break;
+          case 'DOWN':
+            head.row = (head.row + 1) % ROWS;
+            break;
+          case 'LEFT':
+            head.col = (head.col - 1 + COLS) % COLS;
+            break;
+          case 'RIGHT':
+            head.col = (head.col + 1) % COLS;
+            break;
+          default:
+            break;
+        }
+
+        newSnake.unshift(head);
+
+        if (head.row === food.row && head.col === food.col) {
+          setFood(generateFood());
+          setScore(score + 10);
+        } else {
+          newSnake.pop();
+        }
+
+        if (checkCollision(newSnake)) {
+          setGameOver(true);
+        } else {
+          setSnake(newSnake);
+        }
+      };
+
+      const gameInterval = setInterval(moveSnake, 100);
+
+      return () => {
+        clearInterval(gameInterval);
+      };
+    }
+  }, [snake, direction, food, gameOver, isPass, score]);
+
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      switch (e.key) {
+        case 'ArrowUp':
+          setDirection('UP');
+          break;
+        case 'ArrowDown':
+          setDirection('DOWN');
+          break;
+        case 'ArrowLeft':
+          setDirection('LEFT');
+          break;
+        case 'ArrowRight':
+          setDirection('RIGHT');
+          break;
+        default:
+          break;
+      }
+    };
+    document.addEventListener('keydown', handleKeyPress);
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Snake Game</h1>
+      <div className="game-board">
+        {Array.from({ length: ROWS }).map((_, rowIndex) => (
+          <div key={rowIndex} className="row">
+            {Array.from({ length: COLS }).map((_, colIndex) => (
+              <div
+                key={colIndex}
+                className={`cell ${snake.some((segment) => segment.row === rowIndex && segment.col === colIndex) ? 'snake' : ''} ${food.row === rowIndex && food.col === colIndex ? 'food' : ''
+                  }`}
+              ></div>
+            ))}
+          </div>
+        ))}
+      </div>
+      {gameOver && (
+        <div className='dialog'>
+          <div className='reset'>
+            <p>Game Over! <b>Your Score 🐍 {score}</b></p>
+            <button onClick={resetGame}>Restart</button>
+          </div>
+        </div>
+      )}
+      <div className='control'>
+        <button onClick={() => { setIsPass(!isPass) }}> {isPass ? 'Resume' : 'Pass'}</button>
+        <p> Score : 🚀 {score}</p>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
